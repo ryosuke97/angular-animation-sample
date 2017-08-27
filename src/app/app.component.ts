bc19db88786491e3bfbd0c1445069f578c37ca5e
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2017-08-27 23:01:36 +0900

    feat:スケール変更アニメーション追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [trigger('myAnimation', [state('small', style({transform: 'scale(1)',})), state('large', style({transform: 'scale(1.2)',})), transition('small => large', animate('100ms ease-in')), ]), ]
 })
 export class AppComponent {
   title = 'app';
+
+  state: string = 'small';
+  animateMe() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
