a76487d54bf58fc05ae2445d3108ae9f6ed74666
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2017-08-28 22:56:33 +0900

    feat:上下のアニメーション追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,35 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [trigger('myAnimation', [state('small', style({transform: 'scale(1)',})), state('large', style({transform: 'scale(1.2)',})), transition('small => large', animate('100ms ease-in')), ]), ]
+  animations: [trigger(
+                'testAnimation',
+                [
+                  state('small', style({transform: 'scale(1)',})),
+                  state('large', style({transform: 'scale(1.5)',})),
+                  state('up', style({transform: 'translateY(0)'})),
+                  state('down', style({transform: 'translateY(40px)'})),
+                  transition(
+                    'small <=> large',
+                    animate('300ms ease-in', )
+                  ),
+                  transition(
+                    'up <=> down',
+                    animate('300ms ease-in',)
+                  ),
+                ]
+              )],
 })
 export class AppComponent {
   title = 'app';
 
-  state: string = 'small';
-  animateMe() {
-    this.state = (this.state === 'small' ? 'large' : 'small');
+  scaleState: string = 'small';
+  upAndDownState: string = 'up';
+
+  scaleAnimate() {
+    this.scaleState = (this.scaleState === 'small' ? 'large' : 'small');
+  }
+
+  upAndDownAnimate() {
+    this.upAndDownState = (this.upAndDownState === 'up' ? 'down' : 'up');
   }
 }
