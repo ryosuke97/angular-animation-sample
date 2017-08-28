6f85dc63bd054569af0877d61160f793d828f567
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2017-08-29 00:05:31 +0900

    feat:左右のアニメーション追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
                   state('large', style({transform: 'scale(1.5)',})),
                   state('up', style({transform: 'translateY(0)'})),
                   state('down', style({transform: 'translateY(40px)'})),
+                  state('left', style({transform: 'translateX(0)', opacity: 1,})),
+                  state('right', style({transform: 'translateX(100%)', opacity: 0.5,})),
                   transition(
                     'small <=> large',
                     animate('300ms ease-in', )
@@ -19,6 +21,10 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
                   transition(
                     'up <=> down',
                     animate('300ms ease-in',)
+                  ),
+                  transition(
+                    'left <=> right',
+                    animate('1000ms ease-in',)
                   ),
                 ]
               )],
@@ -28,6 +34,7 @@ export class AppComponent {
 
   scaleState: string = 'small';
   upAndDownState: string = 'up';
+  leftAndRightState: string = 'left';
 
   scaleAnimate() {
     this.scaleState = (this.scaleState === 'small' ? 'large' : 'small');
@@ -35,5 +42,9 @@ export class AppComponent {
 
   upAndDownAnimate() {
     this.upAndDownState = (this.upAndDownState === 'up' ? 'down' : 'up');
+  }
+
+  leftAndRightAnimate() {
+    this.leftAndRightState = (this.leftAndRightState === 'left' ? 'right' : 'left')
   }
 }
