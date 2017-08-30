b3653582aeee9ed33b9303c389a28685d9e0a9f2
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2017-08-30 13:51:59 +0900

    feat:表示非表示のアニメーション追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,15 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
   animations: [trigger(
                 'testAnimation',
                 [
-                  state('small', style({transform: 'scale(1)',})),
-                  state('large', style({transform: 'scale(1.5)',})),
-                  state('up', style({transform: 'translateY(0)'})),
-                  state('down', style({transform: 'translateY(40px)'})),
-                  state('left', style({transform: 'translateX(0)', opacity: 1,})),
-                  state('right', style({transform: 'translateX(100%)', opacity: 0.5,})),
+                  state('small', style({transform: 'scale(1)', backgroundColor: '#eee'})),
+                  state('large', style({transform: 'scale(1.5)', backgroundColor: '#3F51B5', color: '#fff'})),
+                  state('up', style({transform: 'translateY(0)', backgroundColor: '#eee',})),
+                  state('down', style({transform: 'translateY(40px)', backgroundColor: '#3F51B5', color: '#fff'})),
+                  state('left', style({transform: 'translateX(0)', opacity: 1, backgroundColor: '#eee'})),
+                  state('right', style({transform: 'translateX(100%)', opacity: 0.5, backgroundColor: '#3F51B5', color: '#fff'})),
+                  state('appear', style({backgroundColor: '#eee'})),
+                  state('disappear', style({height: 0, padding: 0})),
+
                   transition(
                     'small <=> large',
                     animate('300ms ease-in', )
@@ -24,7 +27,12 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
                   ),
                   transition(
                     'left <=> right',
-                    animate('1000ms ease-in',)
+                    animate('300ms ease-in',)
+                  ),
+                  transition(
+                    'appear <=> disappear', [
+                      animate(1000)
+                    ]
                   ),
                 ]
               )],
@@ -35,6 +43,7 @@ export class AppComponent {
   scaleState: string = 'small';
   upAndDownState: string = 'up';
   leftAndRightState: string = 'left';
+  disappearState: string = 'appear';
 
   scaleAnimate() {
     this.scaleState = (this.scaleState === 'small' ? 'large' : 'small');
@@ -46,5 +55,9 @@ export class AppComponent {
 
   leftAndRightAnimate() {
     this.leftAndRightState = (this.leftAndRightState === 'left' ? 'right' : 'left')
+  }
+
+  disappearAnimate() {
+    this.disappearState = (this.disappearState === 'appear' ? 'disappear' : 'appear')
   }
 }
